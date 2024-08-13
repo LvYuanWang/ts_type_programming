2c12b946bc2e5d82828afa31c1ddcb01db9f83ea
LvYuanWang <1175430934@qq.com>
2024-08-13 15:40:59 +0800

    feat: 添加in运算符的使用及联合keyof加泛型配合使用

--- README.md
+++ README.md
@@ -1,55 +1,82 @@
-## `keyof`
+## `in` 运算符遍历
 
-`keyof` 操作符。它可以将对象中的所有键转换为对应字面量类型，然后再组合成联合类型。
+前面讲了in运算符在Typescript可以用来检查属性，在控制流中实现对类型的守卫。
+
+除了类型守卫的作用，`in` 运算符还能遍历联合类型的每一个成员类型
+
+```typescript
+type U = 'a'|'b'|'c';
+
+type Foo = {
+  [key in U]: string;
+};
+// 等同于
+// type Foo = {
+//   a: string,
+//   b: string,
+//   c: string
+// };
+```
+
+上面的讲解`keyof`的时候，不是用到了这样的写法
+
+```typescript
+type A = keyof Person;
+```
+
+那完全可以把`keyof`放入到索引中使用
 
 ```typescript
 type User = {
-  id: number
+  readonly id: number,
   name: string
-  age: number
+  tel: string
+  address?: string
 }
 
-type UserKeys = keyof User // keyof User = 'id' | 'name' | 'age'
-```
+type CopyUser = {
+  [key in keyof User]: User[key]
+}
 
-在 VS Code 中悬浮鼠标只能看到 `keyof User`，看不到其中的实际值，你可以这么做
-
-```typescript
-type UserKeys = keyof User & {} // "id" | "name" | "age"
-```
-
-甚至我们可以结合这`typeof`，直接从一个对象上，获取这个对象键的所有联合类型
-
-```typescript
-const user = {
+const u: CopyUser = {
   id: 1,
-  name: 'hayes',
-  age: 19
+  name: "aaa",
+  tel: "123456",
+  address: "beijing"
 }
-type UserKeys = keyof typeof user // "id" | "name" | "age"
 ```
 
-也可以和方括号运算符结合
+现在固定了`keyof User`，那么我们可以使用泛型，增加一般性
 
 ```typescript
-type Person = {
-  age: number;
-  name: string;
-  sex: boolean;
+type Copy<T> = {
+  [key in keyof T]: T[key]
 };
 
-// number|string|boolean
-type A = Person[keyof Person];
-```
-
-结合着泛型，方括号运算符以及extends受限的泛型，可以直接重写之前我们在重载中写过的代码：
-
-```typescript
-type TagName = keyof HTMLElementTagNameMap;
-
-function createElement<T extends TagName>(tag: T): HTMLElementTagNameMap[T] { 
-  return document.createElement(tag);
+const u: Copy<User> = {
+  id: 1,
+  name: "aaa",
+  tel: "123456",
+  address: "beijing"
 }
 
-const a = createElement("a"); // ok
+type Animal = {
+  name: string,
+  age: number,
+  color: string,
+  type: string
+}
+
+const dog: Copy<Animal> = {
+  name: "jack",
+  age: 3,
+  color: "black",
+  type: "dog"
+}
 ```
+
+> **注意：** **`keyof T`** 这两个的结合得到的是一个联合类型`string | number | symbol`，因为`T`是泛型，并不知道`T`类型的每个键到底是什么类型，可以看一下`keyof any`的结果
+>
+> 当然`[key in keyof T]`现在这样写没有什么问题，但是如果后面要和as，和模板字符串类型连用的话，要注意类型的转换，最好直接让键就是`string`类型
+>
+> `[key in keyof T & string]`
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,95 @@
-type User = {
-  id: number,
-  name: string,
-  sex: '男' | '女'
+type U = 'a' | 'b' | 'c';
+type Foo = {
+  [key in U]: string;
 }
-
-// 小技巧: 通过 & {} 可以看到keyof组成的联合类型
-type UserKeyof = keyof User & {}; // "id" | "name" | "sex"
-
-let a: UserKeyof = 'name';  // "id" | "name" | "sex"
-
-const person = {
-  name: 'Joker',
-  age: 20,
-  sex: '男'
-}
-
-// 可以和 typeof 联合使用, 获取一个对象的键的联合类型
-type PersonKeys = keyof typeof person;  // "name" | "sex" | "age"
-
-// 可以和方括号运算符联合使用, 获取对象类型值的类型
-type Person = {
-  name: string,
-  age: number,
-  isAdult: boolean
-}
-
-type PersonValueType = Person[keyof Person];  // string | number | boolean
-
-// 重载 createElement
-
-// function createElement(tagName: 'div'): HTMLDivElement;
-// function createElement(tagName: 'span'): HTMLSpanElement;
-// function createElement(tagName: string): HTMLElement {
-//   return document.createElement(tagName);
+// 相当于
+// type Foo = {
+//   a: string;
+//   b: string;
+//   c: string;
 // }
 
-// 但是这样写太麻烦了, 我们可以通过 keyof 来简化, 使用 TS 写好的HTMLElementTagNameMap类型
-type TagName = keyof HTMLElementTagNameMap & {};
-
-function createElement<T extends TagName>(tagName: T): HTMLElementTagNameMap[T] {
-  return document.createElement(tagName);
+let foo: Foo = {
+  a: 'a',
+  b: 'b',
+  c: 'c'
 }
 
-// createElement('layer');  // 报错, 因为layer不是合法的标签名, 类型"layer"的参数不能赋给类型"keyof HTMLElementTagNameMap"的参数
-const div = createElement('div');  // HTMLDivElement
-const span = createElement('span');  // HTMLSpanElement
-const canvas = createElement('canvas');  // HTMLCanvasElement
+type Person = {
+  name: string;
+  age: number;
+}
+
+type PersonKeys = keyof Person & {};  // "name" | "age"
+
+type Person2 = {
+  [key in PersonKeys]: Person[key];
+}
+
+type User = {
+  readonly id: number;
+  name: string;
+  tel: string;
+  address?: string;
+}
+
+type UserKeys = keyof User & {};  // "id" | "name" | "tel" | "address"
+
+// 我们发现前面 User 对象中 readonly修饰符 记忆 可读属性 没有了
+type CopyUser = {
+  [key in UserKeys]: User[key];
+}
+
+let u1: CopyUser = {
+  id: 3,
+  name: 'Anna',
+  tel: '12233445566',
+  address: '上海'
+}
+u1.id = 2;
+
+// 解决办法直接使用 in keyof User 的方法
+type CopyUser2 = {
+  [key in keyof User]: User[key];
+}
+
+let u2: CopyUser2 = {
+  id: 4,
+  name: 'Jack',
+  tel: '133333444'
+}
+// u2.id = 5;  // 无法赋值给只读属性
+
+// 使用 泛型 再次优化、升级
+// 限制传入的 T 只能是 对象类型
+type Copy<T extends object> = {
+  [key in keyof T]: T[key];
+}
+
+let u3: Copy<User> = {
+  id: 5,
+  name: 'Joker',
+  tel: '1555566666'
+}
+// u3.id = 6;  // 无法赋值给只读属性
+
+// type AnimalKeys = keyof Animal;
+// const animalKeys: Copy<AnimalKeys> = {} // 类型“string”不满足约束“object”: 不能将类型“string”分配给类型“object”
+
+type Animal = {
+  name: string,
+  age: number,
+  color: string,
+  type: 'cat' | 'dog'
+}
+const dog: Copy<Animal> = {
+  name: '小白',
+  age: 4,
+  color: 'white',
+  type: "dog"
+}
+
+// keyof T 键名的类型可以得到一个联合类型 string | number | symbol
+type A = keyof any; // string | number | symbol
+// 后面的映射类型, 可能会联合模板字符串一起操作, 可能会要求 keyof any 得到的必须是 string 类型
+type B = keyof any & string;  // string
