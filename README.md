96d119fbed11829924dfde0dd90561330535244d
LvYuanWang <1175430934@qq.com>
2024-08-15 23:21:47 +0800

    feat: 新增TS方括号运算符常见的操作用途

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-### 关联泛型
+## 方括号运算符常见操作
 
-如果现在希望实现这么一个效果，在原有对象类型的属性上进行挑选，根据挑选属性的结果，形成新的类型
+### 获取值的类型
 
 ```typescript
 type User = {
@@ -10,29 +10,65 @@ type User = {
   address?: string
 }
 
-// 比如挑选name和tel属性,形成下面的类型
+// type ValueType = User['id' | 'name'];
+type ValueType = User[keyof User];
 
-type UserPick = {
-  name: string
-  tel: string
+// 泛型
+type MyReadonly<T> = {
+  +readonly [key in keyof T]: T[key]
 }
 ```
+
+数组一样可以处理
 
 ```typescript
-type MyPick<T, K extends keyof T> = {
-  [key in K]: T[key]
-}
-
-type Admin = MyPick<User, "name"| "tel">;
-
-const u: Admin = {
-  name: "aaa",
-  tel: "123456"
-}
+const arr = ["admin", "user", "client"];
+type ArrType = typeof arr[number]; // string
 ```
 
-**关键点在于：**
+将上面的数组通过`as const`转为只读元组类型之后，得到的是具体字面量类型的联合
 
-1、确定需要的泛型参数个数
+```typescript
+const arr = ["admin", "user", "client"] as const;
+type ArrType = typeof arr[number]; // "admin" | "user" | "client"
+```
 
-2、第二个泛型参数的类型应该来源于第一个参数
+当然，我们也能写成泛型工具
+
+```typescript
+type ArrType<T extends readonly any[]> = T[number];
+type A = ArrType<["admin", "user", "client"]>;
+```
+
+### 获取数组的长度
+
+可以通过`['length']`获取元组类型的具体长度number字面量类型，注意如果仅仅是数组，只能获取number类型
+
+```typescript
+const arr = ["admin", "user", "client"] as const;
+type Len = typeof arr['length'] 
+let n:Len = 3
+```
+
+同样也能写成泛型工具：
+
+```typescript
+type ArrLen<T extends readonly any[]> = T['length'];
+type B = ArrLen<[1, 2, 3, 4, 5, 6]>; //6
+```
+
+### 结合泛型使用扩展运算符
+
+比如现在希望写一个泛型工具，实现两个元组类型的拼接
+
+```typescript
+type Result = Concat<[1,2],[3,4]>; //[1,2,3,4]
+```
+
+咋一看没啥思路，但是其实ts和js一样，支持`... Spread扩展运算符`
+
+```typescript
+type Concat<T extends any[], U extends any[]> = [...T, ...U];
+type C = Concat<[1, 2, 3, 4], ["a", "b", "c"]>;
+```
+
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,43 @@
-// 关联泛型
 type User = {
   readonly id: number,
   name: string,
   tel: string,
-  address?: string
+  address?: string,
+  gender: boolean
 }
 
-// 比如挑选 name 和 tel 属性, 形成下面的类型
+// type ValueType = User['id' | 'name']; // string | number
+type ValueType = User[keyof User]
 
-type UserPick = {
-  name: string,
-  tel: string
+// 泛型
+type MyReadonly<T> = {
+  readonly [key in keyof T]: T[key]
 }
 
-// function myPick(obj, key) {
-//   // todo ...
-//   return {}
-// }
+// 数组
+const arr = ['admin', 'user', 'client'] as const;
+const arr2 = [1, true, 'admin'];
 
-type MyPick<T, K extends keyof T> = {
-  [key in K]: T[key];
-}
-let pick: Pick<User, "name" | "address">;
+// type ArrType = typeof arr[number];  // "admin" | "user" | "client"
+// type Arr2Type = typeof arr2[number];  // string | number | boolean
+// type FirstArrType = typeof arr2[0]; // string | number | boolean
+// type LastArrType = typeof arr[2]; // "client"
 
-let u: MyPick<User, "name" | "tel"> = {
-  name: 'Jack',
-  tel: '1544466'
-}
+// 获取元组类型的泛型工具
+type ArrType<T extends readonly any[]> = T[number];
+// type A = ArrType<["admin", "user", "client"]>  // "admin" | "user" | "client"
+type A = ArrType<typeof arr2> // string | number | boolean
 
+// 获取数组长度 length
+type Len = typeof arr['length'];  // 3
+type LenType = typeof arr2['length']; // number
 
-type PickUser = MyPick<User, "id" | "name" | "address">;
+// 获取数组长度的泛型工具
+type ArrLen<T extends readonly any[]> = T['length'];
+// 直接写一个数组就会认为这个数组是元组
+type B = ArrLen<[1, 2, 3, 4, 5, 6]>;  // 6
 
-let u2: PickUser = {
-  id: 3,
-  name: 'Joker',
-  // address: '南通'
-}
-// u2.id = 4;  // 无法为只读属性赋值
+// 泛型 + 扩展运算符 ... + 方括号运算符 []
+// 注意: 扩展运算符只能用于数组
+type Concat<T extends any[], U extends any[]> = [...T, ...U];
+type Result = Concat<[1, true], ["admin", null]>; // [1, true, "admin", null]
